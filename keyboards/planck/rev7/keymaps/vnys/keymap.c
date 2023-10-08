437f706837778cc95215b8a9f363e5de496bda44
Victor Nystad <vnys@equinor.com>
2023-10-08 10:13:00 +0200

    🔧 Add numbers layer

--- keyboards/planck/rev7/keymaps/vnys/keymap.c
+++ keyboards/planck/rev7/keymaps/vnys/keymap.c
@@ -16,7 +16,7 @@
 
 #include QMK_KEYBOARD_H
 
-enum planck_layers { _QWERTY, _FN, _LOWER, _RAISE, _ADJUST };
+enum planck_layers { _QWERTY, _FN, _NUMBERS, _LOWER, _RAISE, _ADJUST };
 
 enum planck_keycodes { QWERTY = SAFE_RANGE, BACKLIT, EXT_PLV };
 
@@ -42,22 +42,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #define OSLASH KC_SCLN
 #define AELIG KC_QUOT
 #define FN MO(_FN)
+#define TG_NUMB TG(_NUMBERS)
+#define TO_QWRT TO(_QWERTY)
 
 [_QWERTY] = LAYOUT_planck_2x2u(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    ARING,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    OSLASH,  AELIG,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    _______, KC_LCTL, KC_LALT, KC_LGUI,    KC_ENT,           KC_SPC,        FN,      KC_RGUI, KC_RALT, KC_RCTL
+    TG_NUMB, KC_LCTL, KC_LALT, KC_LGUI,    KC_ENT,           KC_SPC,        FN,      KC_RGUI, KC_RALT, KC_RCTL
 ),
 
 
 [_FN] = LAYOUT_planck_2x2u(
-    _______,  _______,  KC_UP,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_BSPC,
-    _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-    QK_BOOT,  _______,  _______,  _______,       _______,          _______,         _______,  _______,  _______,  _______
+    _______,  _______,  KC_UP,    _______,  _______,  _______,  _______,  _______,  _______,  KC_KB_VOLUME_DOWN,  KC_KB_VOLUME_UP,  KC_BSPC,
+    _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  _______,  _______,  _______,  _______,  _______,  _______,            _______,          _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MUTE,  _______,  _______,            _______,          _______,
+    QK_BOOT,  _______,  _______,  _______,       _______,          _______,         _______,  _______,            _______,          _______
 ),
 
+[_NUMBERS] = LAYOUT_planck_2x2u(
+    _______,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,  _______,  _______,       _______,          _______,         _______,  _______,  _______,  _______
+),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
