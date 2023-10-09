3346db19a3fe0e191b86819d88478df3f9725863
Victor Nystad <vnys@equinor.com>
2023-10-09 22:57:32 +0200

    🔧 Add num pad and @

--- keyboards/planck/rev7/keymaps/vnys/keymap.c
+++ keyboards/planck/rev7/keymaps/vnys/keymap.c
@@ -62,12 +62,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #define TO_NUMB TG(_NUMBERS)
 #define TO_QWRT TO(_QWERTY)
 #define ONESHOTS OSL(_ONESHOTS)
+#define LOWER MO(_LOWER)
+
 
 [_QWERTY] = LAYOUT_planck_2x2u(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    ARING,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    OSLASH,  AELIG,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    TO_NUMB, KC_LCTL, KC_LALT, KC_LGUI,    KC_ENT,           KC_SPC,        FN,      KC_RGUI, KC_RALT, ONESHOTS
+    _______, KC_LCTL, KC_LALT, KC_LGUI,    LOWER,            KC_SPC,        FN,      KC_RGUI, KC_RALT, ONESHOTS
 ),
 
 
@@ -103,11 +105,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-[_LOWER] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+[_LOWER] = LAYOUT_planck_2x2u(
+    _______,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_4,     KC_5,     KC_6,     _______,  KC_NUHS,
+    _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_1,     KC_2,     KC_3,     _______,  _______,
+    _______,  _______,  _______,  _______,       _______,          KC_0,            _______,  _______,  _______,  _______
 ),
 
 /* Raise
--- keyboards/planck/rev7/keymaps/vnys/readme.md
+++ keyboards/planck/rev7/keymaps/vnys/readme.md
@@ -3,8 +3,11 @@
 A Norwegian 2x2u layout for mac using dedicated keys for the letter ÆØÅ on the base layer.
 The characters are placed (for the most part) where my fingers expect to find them, coming from an Iqunix F60 keyboard. That means WASD arrow keys on the same layer as volume up/down etc. Since the character Å is in the same location as backspace, I’ve moved backspace to the same layer.
 After going bananas with an EZ Planck keyboard I realized it’s better to keep some things the same, even though the ergonomics might not be perfect.
-That also means keyboard shortcuts on the number layer are easier to perform with a toggle to the number layer instead of adding another finger to the modifier combo. The number layer key is at the bottom right corner, so it can be hit with the palm of the left hand.
+That also means keyboard shortcuts on the number layer are easier to perform with a toggle to the number layer instead of adding another finger to the modifier combo. The number layer toggle key is at the bottom right corner, so it can be hit with the palm of the left hand.
+
+The enter and backspace keys are combos, io and kl respectively
 
 ## Some things I want to explore:
 
 1. Adding a sound to each layer, so when toggling the number layer for example, there should be a distinct melody that informs me that I’m in that layer. I also want to use a key to play the melody of the layer I’m in, in case I’m wondering where I am.
+2. Backspace needs to be in the base layer for
