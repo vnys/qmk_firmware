e7812697640c10829ee02f67b218afed9bc0b56f
Victor Nystad <vnys@equinor.com>
2023-10-10 09:16:03 +0200

    🔧 Add homerow shifts

--- keyboards/planck/rev7/keymaps/vnys/config.h
+++ keyboards/planck/rev7/keymaps/vnys/config.h
@@ -15,12 +15,13 @@
  */
 
 #pragma once
+// #define AUDIO_ENABLE
 
 #ifdef AUDIO_ENABLE
-#    define STARTUP_SONG SONG(PLANCK_SOUND)
+#define STARTUP_SONG SONG(PLANCK_SOUND)
 // #define STARTUP_SONG SONG(NO_SOUND)
 
-#    define DEFAULT_LAYER_SONGS \
+#define DEFAULT_LAYER_SONGS \
         { SONG(QWERTY_SOUND), SONG(COLEMAK_SOUND), SONG(DVORAK_SOUND) }
 #endif
 
@@ -32,7 +33,7 @@
    - MIDI notes can be sent when in Music mode is on
 */
 
-#define MIDI_BASIC
+// #define MIDI_BASIC
 
 /* enable advanced MIDI features:
    - MIDI notes can be added to the keymap
@@ -43,3 +44,4 @@
 // #define MIDI_ADVANCED
 
 #define COMBO_TERM 25
+#define BILATERAL_COMBINATIONS
--- keyboards/planck/rev7/keymaps/vnys/keymap.c
+++ keyboards/planck/rev7/keymaps/vnys/keymap.c
@@ -16,6 +16,7 @@
 
 #include QMK_KEYBOARD_H
 
+
 enum combos {
     COMBO_BACKSPACE,
     COMBO_ENTER,
@@ -64,10 +65,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #define ONESHOTS OSL(_ONESHOTS)
 #define LOWER MO(_LOWER)
 
+// HOMEROW SHIFT
+#define F_SFT LSFT_T(KC_F)
+#define J_SFT RSFT_T(KC_J)
 
 [_QWERTY] = LAYOUT_planck_2x2u(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    ARING,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    OSLASH,  AELIG,
+    KC_TAB,  KC_A,    KC_S,    KC_D,    F_SFT,   KC_G,    KC_H,    J_SFT,   KC_K,    KC_L,    OSLASH,  AELIG,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
     _______, KC_LCTL, KC_LALT, KC_LGUI,    LOWER,            KC_SPC,        FN,      KC_RGUI, KC_RALT, ONESHOTS
 ),
